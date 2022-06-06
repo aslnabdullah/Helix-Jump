6d0fa508bc9485371ceb3c7b59b798803542a55c
aslnabdullah <abdullahaslan2510@gmail.com>
2022-06-06 22:11:08 +0300

    Initial commit

---
+++ Assets/scripts/RotateMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMove : MonoBehaviour
+{
+    public float rotateSpeed;
+    private float MoveX;
+
+    void Update()
+    {
+        MoveX = Input.GetAxis("Mouse X");
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(0f, MoveX * rotateSpeed * Time.deltaTime, 0f);
+
+        }
+        
+    }
+}
